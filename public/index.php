33e113316117c36d3ead1d5b58b868bbb4ecc41c
batuhansunucu <m.batuhan.tasdemir@gmail.com>
2022-02-13 22:36:17 +0300

    your

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
 
 /*
 |--------------------------------------------------------------------------
-| Register The Auto Loader
+| Register The Auto Loader1
 |--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
